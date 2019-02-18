fa60416f63990a576fc32294db87f5ec77a47976
boubouhkarim <boubouh.karim@gmail.com>
2019-02-18 19:33:41 +0100

    Fork & wait

--- fork.c
+++ fork.c
@@ -1,4 +1,3 @@
-// libcstar procedures for printing
 void init_library();
 void print(uint64_t *s);
 void print_character(uint64_t c);
@@ -8,57 +7,31 @@ void print_hexadecimal(uint64_t n, uint64_t a);
 void print_octal(uint64_t n, uint64_t a);
 void print_binary(uint64_t n, uint64_t a);
 void println();
-
 uint64_t *foo;
 
 uint64_t main()
 {
     uint64_t pid;
-    uint64_t pid2;
-    uint64_t i;
-    uint64_t* status;
-    
-    // initialize selfie's libcstar library
     init_library();
-    pid = fork();
-
-    if (pid != 0)
-    {
-        // parent
-        wait(status);
-        print("Parent PID :     ");
+    pid= fork();
+    if(pid==0){
         print_integer(pid);
-        println();
-
-        pid2 = fork();
-        if (pid2 != 0)
-        {
-            // parent
-            wait(status);
-            print("Parent 2 PID :     ");
-            print_integer(pid2);
-            println();
+        foo = "Hello World!    ";
+        while (*foo != 0) {
+            write(1, foo, 8);
+            foo = foo + 1;
         }
-        else
-        {
-            // child
-            print("Child 2 PID :     ");
-            print_integer(pid2);
-            println();
-        }
-    }else{
-        // child
-        print("Child PID :     ");
+    } else{
+        foo = "Hello World!    ";
+        print("Child PID : ");
         print_integer(pid);
-        println();
-    }
-  
- foo = "Hello World!    ";
-    while (*foo != 0)
-    {
-        write(1, foo, 8);
+        print("\n");
 
-        foo = foo + 1;
+        while (*foo != 0)
+        {
+            write(1, foo, 8);
+            foo = foo + 1;
+        }
+        print("\n");
     }
 }
-
